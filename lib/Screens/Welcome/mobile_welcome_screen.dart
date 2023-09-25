7ce0834da451dc8bc8691549c004f672561ff042
gabrielespanhol <gabryel.ehs@gmail.com>
2023-09-25 17:26:16 -0300

    ajuste

---
+++ lib/Screens/Welcome/mobile_welcome_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MobileWelcomeScreen extends StatelessWidget {
+  const MobileWelcomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
---
+++ lib/Screens/Welcome/web_welcome_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebWelcomeScreen extends StatelessWidget {
+  const WebWelcomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row();
+  }
+}
